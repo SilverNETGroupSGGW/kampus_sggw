75c61f7e5570d915a89e7eb64543dcc032cf0afe
martamazurkiewicz <marta@mazurkiewi.cz>
2021-04-06 12:00:31 +0200

    Added ModalBottomSheet

--- lib/screens/map_screen/map_floating_buttons.dart
+++ lib/screens/map_screen/map_floating_buttons.dart
@@ -7,17 +7,35 @@ class MapFloatingButtons extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.end,
       children: [
         FloatingActionButton(
-          child: const Icon(Icons.map),
-          backgroundColor: Colors.lightGreen,
-        ),
+            child: const Icon(Icons.map),
+            backgroundColor: Colors.lightGreen,
+            onPressed: () {
+            }),
         Padding(
           padding: EdgeInsets.all(5),
         ),
         FloatingActionButton(
-          child: const Icon(Icons.search),
-          backgroundColor: Colors.green,
-        ),
+            child: const Icon(Icons.search),
+            backgroundColor: Colors.green,
+            onPressed: () => _onSearchButtonPressed(context)),
       ],
     );
   }
+  void _onSearchButtonPressed(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (context) {
+        return Column(
+          children: <Widget>[
+            ListTile(
+              leading: Icon(Icons.ac_unit),
+              title: Text('Cooling'),
+              onTap: () => {},
+            ),
+          ],
+        );
+      },
+    );
+  }
+  
 }
