e4e605cf903ab83d9e991b0168fb096431ed1864
Jakub Ćwikowski <kcwikowski007@yahoo.com>
2021-04-16 12:04:27 +0200

    Add padding to SimpleDialog

--- lib/screens/map_screen/service_card.dart
+++ lib/screens/map_screen/service_card.dart
@@ -16,50 +16,53 @@ class ServiceCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SimpleDialog(
-      title: Text(service.name),
-      children: [
-        if (service.photoPath != null)
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: SimpleDialog(
+        title: Text(service.name),
+        children: [
+          if (service.photoPath != null)
+            Padding(
+              padding: const EdgeInsets.only(bottom: 8.0),
+              child: ClipRect(child: Image.asset(service.photoPath)),
+            ),
           Padding(
-            padding: const EdgeInsets.only(bottom: 8.0),
-            child: ClipRect(child: Image.asset(service.photoPath)),
+            padding: const EdgeInsets.only(
+              bottom: 15,
+              left: 22,
+              right: 22,
+              top: 5,
+            ),
+            child: Text(service.description),
           ),
-        Padding(
-          padding: const EdgeInsets.only(
-            bottom: 15,
-            left: 22,
-            right: 22,
-            top: 5,
-          ),
-          child: Text(service.description),
-        ),
-        if (service.url != null)
-          Padding(
-              padding: const EdgeInsets.only(left: 22, right: 22, bottom: 8.0),
-              child: GestureDetector(
-                  onTap: _goToServiceURL,
-                  child: RichText(
-                    text: TextSpan(
-                      children: [
-                        WidgetSpan(
-                          child: Padding(
-                            padding: const EdgeInsets.only(right: 4.0),
-                            child: Icon(Icons.language,
-                                size: 20, color: Colors.blue),
+          if (service.url != null)
+            Padding(
+                padding: const EdgeInsets.only(left: 22, right: 22, bottom: 8.0),
+                child: GestureDetector(
+                    onTap: _goToServiceURL,
+                    child: RichText(
+                      text: TextSpan(
+                        children: [
+                          WidgetSpan(
+                            child: Padding(
+                              padding: const EdgeInsets.only(right: 4.0),
+                              child: Icon(Icons.language,
+                                  size: 20, color: Colors.blue),
+                            ),
                           ),
-                        ),
-                        TextSpan(
-                            text: 'Strona Internetowa',
-                            style: TextStyle(
-                              fontSize: 16,
-                              color: Colors.blue,
-                              decoration: TextDecoration.underline,
-                            )),
-                      ],
-                    ),
-                  ))),
-      ],
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(18.0)),
+                          TextSpan(
+                              text: 'Strona Internetowa',
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: Colors.blue,
+                                decoration: TextDecoration.underline,
+                              )),
+                        ],
+                      ),
+                    ))),
+        ],
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(18.0)),
+      ),
     );
   }
 }
