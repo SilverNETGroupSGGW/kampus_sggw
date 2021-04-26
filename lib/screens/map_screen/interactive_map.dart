ce82b02d525307135ea1e0f7e22ecd056a8a1bc9
Kamil Mikołajczuk <k.mikolajczuk123@gmail.com>
2021-04-26 21:22:02 +0200

    osm

--- lib/screens/map_screen/interactive_map.dart
+++ lib/screens/map_screen/interactive_map.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong/latlong.dart';
 import 'package:kampus_sggw/models/map_item.dart';
 import 'location_pin.dart';
 import 'package:kampus_sggw/models/location.dart';
@@ -34,26 +36,30 @@ class _InteractiveMapState extends State<InteractiveMap> {
 
   @override
   Widget build(BuildContext context) {
-    return InteractiveViewer(
-      constrained: false,
-      minScale: widget.map.minScale,
-      maxScale: widget.map.maxScale,
-      child: Stack(
-        children: [
-          Image(
-            image: AssetImage("assets/images/map/map_z2.jpg"),
-          ),
-          Positioned.fill(
-            child: Stack(
-              children: widget.pins,
-            ),
-          ),
-        ],
+    return FlutterMap(
+      options: MapOptions(
+        center: LatLng(51.5, -0.09),
+        zoom: 13.0,
       ),
-      onInteractionUpdate: (ScaleUpdateDetails details) {
-        _onScaleUpdate();
-      },
-      transformationController: widget.transController,
+      layers: [
+        TileLayerOptions(
+            urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+            subdomains: ['a', 'b', 'c']
+        ),
+        MarkerLayerOptions(
+          markers: [
+            Marker(
+              width: 80.0,
+              height: 80.0,
+              point: LatLng(51.5, -0.09),
+              builder: (ctx) =>
+                  Container(
+                    child: FlutterLogo(),
+                  ),
+            ),
+          ],
+        ),
+      ],
     );
   }
 
