3de5ac0ed1f27bf763087c83cacf457b68465632
martamazurkiewicz <marta@mazurkiewi.cz>
2021-05-07 14:30:06 +0200

    Added StreamService class for encapsulating streams and sinks

---
+++ lib/logic/stream_service.dart
@@ -0,0 +1,22 @@
+import 'dart:async';
+
+class StreamService{
+  StreamController _streamController = StreamController.broadcast();
+  StreamSubscription _subscription;
+  
+  void dispose() {
+    _streamController.close();
+  }
+
+  void cancelSubscription(){
+    _subscription.cancel();
+  }
+
+  void listen(function){
+    _subscription = _streamController.stream.listen(function);
+  }
+
+  void addEvent(param){
+    param==null ? _streamController.sink.add(null) : _streamController.sink.add(param);
+  }
+}
