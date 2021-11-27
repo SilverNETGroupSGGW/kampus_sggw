1db48a4611bdcea57f713f9a8369d6110c23758e
mmaz <marta@mazurkiewi.cz>
2021-11-27 17:25:45 +0100

    Moved fuzzy logic operations to external class

--- lib/models/map_items.dart
+++ lib/models/map_items.dart
@@ -1,6 +1,5 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:kampus_sggw/logic/event_parameters/suggested_item.dart';
 import 'package:kampus_sggw/models/map_item.dart';
 import 'package:kampus_sggw/models/types/map_item_types/map_item_types.dart';
 import 'package:get_storage/get_storage.dart';
@@ -30,15 +29,9 @@ class MapItems extends ChangeNotifier {
   factory MapItems.fromJson(Map<String, dynamic> json) =>
       _$MapItemsFromJson(json);
 
-  Set<MapItem> filterByFunction(ObjectFunctionGroup functionGroup) {
-    Set<MapItem> filteredItems = {};
-    mapItems.forEach((item) {
-      if (item.doesItemFulfilFunction(functionGroup)) {
-        filteredItems.add(item);
-      }
-    });
-    return filteredItems;
-  }
+  Set<MapItem> filterByFunction(ObjectFunctionGroup functionGroup) =>
+      {}..addAll(
+          mapItems.where((item) => item.doesItemFulfilFunction(functionGroup)));
 
   List<MapItem> getItems(List<int> itemsIds) {
     List<MapItem> tmp = [];
@@ -48,52 +41,10 @@ class MapItems extends ChangeNotifier {
     return tmp;
   }
 
-  MapItem findItemByQuery(String query) {
-    List<SuggestedItem> similarItems = findItemsByQuery(query);
-    return similarItems.length > 0 ? similarItems[0].mapItem : null;
-  }
-
-  MapItem findItemByID(int id) {
-    return mapItems.firstWhere((element) => element.id == id);
-  }
-
-  List<SuggestedItem> findItemsByQuery(String query) {
-    List<SuggestedItem> similarityList =
-        _getSimilarityMapForEachItem(query.toLowerCase());
-    return _getMostSimilarMapItems(similarityList);
-  }
-
-  List<SuggestedItem> _getSimilarityMapForEachItem(String query) {
-    List<SuggestedItem> similarityList = [];
-    mapItems.forEach(
-      (item) {
-        var similarity = item.searchingSet.search(query);
-        if (similarity.length > 0) {
-          similarityList.add(
-            SuggestedItem(
-              mapItem: item,
-              matchedQuery: similarity[0].item,
-              similarity: similarity[0].score,
-            ),
-          );
-        }
-      },
-    );
-    return similarityList;
-  }
-
-  List<SuggestedItem> _getMostSimilarMapItems(
-      List<SuggestedItem> similarityList) {
-    similarityList.sort((a, b) => a.similarity.compareTo(b.similarity));
-    return similarityList.take(6).toList();
-  }
-
   void _bindItemWithTypes(
-      MapItemTypes mapItemTypes, ServiceTypes serviceTypes) {
-    for (var mapItem in mapItems) {
-      mapItem.initializeTypes(mapItemTypes, serviceTypes);
-    }
-  }
+          MapItemTypes mapItemTypes, ServiceTypes serviceTypes) =>
+      mapItems
+          .forEach((item) => item.initializeTypes(mapItemTypes, serviceTypes));
 
   static Future<MapItems> _loadFromJson() async {
     Map<String, dynamic> mapItemsMap = jsonDecode(_getJsonString());
