d4636cac990ea87b2d60f13de919fa4de4e64d93
martamazurkiewicz <marta@mazurkiewi.cz>
2022-01-16 21:02:16 +0100

    Script cannot work without key as argument

--- set_map_key.sh
+++ set_map_key.sh
@@ -2,10 +2,6 @@
 git update-index --skip-worktree android/app/src/main/AndroidManifest.xml
 git update-index --skip-worktree ios/Runner/AppDelegate.swift
 git update-index --skip-worktree web/index.html
-if (($# == 0)); then
-  echo "Requieres option -k with google map key as argument" >&2
-  exit 1
-fi
 key_alias='MAPS_KEY_HERE'
 while getopts k:r: opt; do
   case $opt in
@@ -17,6 +13,10 @@ while getopts k:r: opt; do
       ;;
   esac
 done
+if [ -z "$map_key" ]; then
+  echo 'Missing required Google Map key' >&2
+  exit 1
+fi
 sed -i -e "s/$key_alias/$map_key/g" android/app/src/main/AndroidManifest.xml
 sed -i -e "s/$key_alias/$map_key/g" ios/Runner/AppDelegate.swift
 sed -i -e "s/$key_alias/$map_key/g" web/index.html
