22dcc2dc53685f42bec4ae79d6cb9ddd7995bcbc
Jacob Ćwikowski <kcwikowski007@yahoo.com>
2022-02-26 19:28:34 +0100

    Add new function mapItemsNew to Firebase
    This functions accesses the new json file from firebase storage.

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -31,3 +31,33 @@ export const mapItems = functions.https.onRequest(async (request, response) => {
     response.status(500).send(error)
   }
 })
+
+export const mapItemsNew = functions.https.onRequest(async (request, response) => {
+  const token = request.query.token?.toString() || ''
+
+  try {
+    const result = await getFileUpdate(token, 'map_items_new.json')
+
+    functions.logger.log({
+      requestedToken: token,
+      updateNeeded: result.data.length !== 0,
+    })
+
+    response.send(result)
+  } catch (e) {
+    functions.logger.error({
+      requestedToken: token,
+      error: e,
+    })
+
+    const error: DataResponse = {
+      error: true,
+      oldToken: token,
+      token: '',
+      data: '',
+      message: 'Wystąpił niespodziewany błąd',
+    }
+
+    response.status(500).send(error)
+  }
+})
