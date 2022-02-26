0e66ae257131a20e9693668f02c6d9a9075d0b7c
Wiktor <wijaweq@gmail.com>
2022-02-26 19:09:27 +0100

    Change Switch to SwitchListTile in change_theme_widget

--- lib/screens/settings_screen/settings_screen_widgets/change_theme_widget.dart
+++ lib/screens/settings_screen/settings_screen_widgets/change_theme_widget.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:kampus_sggw/models/theme_model.dart';
+import 'package:kampus_sggw/translations/locale_keys.g.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:provider/provider.dart';
 
 class ChangeThemeWidget extends StatefulWidget {
@@ -20,7 +22,8 @@ class _ChangeThemeWidget extends State<ChangeThemeWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return Switch(
+    return SwitchListTile(
+      title: Text(LocaleKeys.dark_mode.tr()),
       value: _isModeDarkSwitchValue,
       onChanged: (value) {
         _theme.switchTheme();
