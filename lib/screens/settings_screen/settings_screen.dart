e2df263cc75c5655c9c230bbfe46c585f7d18095
Adam B <grzybojad@gmail.com>
2022-03-07 18:18:48 +0100

    Added Authors list to settings

--- lib/screens/settings_screen/settings_screen.dart
+++ lib/screens/settings_screen/settings_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:kampus_sggw/screens/settings_screen/settings_screen_widgets/authors_button.dart';
 import 'package:kampus_sggw/screens/settings_screen/settings_screen_widgets/campus_rules_button.dart';
 import 'package:kampus_sggw/screens/settings_screen/settings_screen_widgets/category_container.dart';
 import 'package:kampus_sggw/screens/settings_screen/settings_screen_widgets/change_language_widget.dart';
@@ -27,6 +28,7 @@ class SettingsScreen extends StatelessWidget {
           CategoryContainer(
             children: [
               CampusRulesButton(),
+              AuthorsButton()
             ],
           ),
         ],
--- lib/screens/settings_screen/settings_screen_widgets/authors_button.dart
+++ lib/screens/settings_screen/settings_screen_widgets/authors_button.dart
@@ -1,23 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:kampus_sggw/screens/rules_screen/rules_loader.dart';
+import 'package:kampus_sggw/screens/creators_screen/creators_screen.dart';
 import 'package:kampus_sggw/translations/locale_keys.g.dart';
 import 'package:easy_localization/easy_localization.dart';
 
-class CampusRulesButton extends StatelessWidget {
-  const CampusRulesButton({Key? key}) : super(key: key);
+class AuthorsButton extends StatelessWidget {
+  const AuthorsButton({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
       title: Text(
-        LocaleKeys.drawer_campus_rules.tr(),
+        LocaleKeys.authors.tr(),
         textAlign: TextAlign.center,
       ),
       onTap: () {
         Navigator.push(
           context,
           MaterialPageRoute(
-            builder: (context) => RulesBuilder(),
+            builder: (context) => CreatorsScreen(),
           ),
         );
       },
